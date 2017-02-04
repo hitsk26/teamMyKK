89070a606760e59656eb04f7b59ec09fef07a379
hhomma <hhomma@yahoo-corp.jp>
2017-02-05 01:20:29 +0900

    fix set volume

--- sound.py
+++ sound.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 #-*- cording: utf-8 -*-
-
 import pygame.mixer
 import time
 
-# mixerモジュールの初期化
 pygame.mixer.init()
-# 音楽ファイルの読み込み
 pygame.mixer.music.load("/home/tsuzuku/python_games/match1.wav")
-# 音楽再生、および再生回数の設定(-1はループ再生)
-pygame.mixer.music.play(-1)
 
-time.sleep(60)
-# 再生の終了
+pygame.mixer.music.play(3)
+pygame.mixer.music.set_volume(0.8)
+
+time.sleep(2)
 pygame.mixer.music.stop()
